7bd0b71978e28bab0da8ce11749738d3a64b0903
AlessioDPT <alessio.recos@gmail.com>
2021-07-16 17:13:30 +0200

    ADD- struttura main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,35 @@
 <template>
-  <main>main area</main>
+  <main>
+
+    <!-- section Our Consultats -->
+    <Consults/>
+
+    <!-- section project -->
+    <Project/>
+
+    <!-- section Testimonial -->
+    <Testimonial/>
+
+    <!-- section blog -->
+    <Blog/>
+
+    
+  </main>
 </template>
 
 <script>
+import Consults from "@/components/Consults"
+import Project from "@/components/Project"
+import Testimonial from "@/components/Testimonial"
+import Blog from "@/components/Blog"
 export default {
-    name:"Main"
+    name:"Main",
+    components: {
+      Consults,
+      Project,
+      Testimonial,
+      Blog
+    }
 }
 </script>
 
