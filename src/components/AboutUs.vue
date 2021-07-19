29c25ef90afdc0015ce4da11661bb1cc13053c90
AlessioDPT <alessio.recos@gmail.com>
2021-07-19 12:19:58 +0200

    ADD- subsection X About us ("StartedSubSec

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,31 +1,34 @@
 <template>
   <section>
       <div class="container-fluid p-0">
-          <div class="row">
+        <div class="row">
 
-          <div class="left_side col">
+            <div class="left_side col">
               <img src="@/assets/images/business-people-working-together-on-project-and-5FHSKBL.jpg" alt="Consulting People">
-          </div>
-          <div class="right_side col px-5 d-flex align-items-center">
+            </div>
+
+            <div class="right_side col px-5 d-flex align-items-center">
               <div class="box_text w-75">
                 <div class="line_titles "></div>
                 <span>ABOUT US</span>
                 <h2>WE MAKE YOUR STRATEGY WORK</h2>
                 <p>When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed when I hear the buzz of the little world among the stalks,and grow familiar indescribable forms of the insects .and flies, then I feel the presence of the Almighty . </p>
               </div>
-              
+            </div>
 
-          </div>
-
-          </div>
+        </div>
+        <StartedSubSec/>
 
       </div>
   </section>
 </template>
 
 <script>
+import StartedSubSec from "@/components/StartedSubSec.vue";
 export default {
-name : "AboutUs"
+name : "AboutUs",
+components: {StartedSubSec
+}
 }
 </script>
 
---
+++ src/components/StartedSubSec.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>ciao</section>
+</template>
+
+<script>
+export default {
+    name: "StartedSubSec"
+
+}
+</script>
+
+<style>
+
+</style>
